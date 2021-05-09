06d9cc07e3bb498dc38dea2408ce99b4527947af
Ali Mohamed <alimohamadyoussef21@gmail.com>
2021-05-10 01:13:32 +0200

    new techniques

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-from random import randint
 
 from matplotlib import pyplot as pplot
 from numpy import random
@@ -26,27 +25,93 @@ def insertion_sort(arr):
             arr[j + 1] = temp
 
 
+def merge(a, b):
+    c = []
+    while a and b:
+        if a[0] > b[0]:
+            c.append(b[0])
+            b.pop(0)
+        else:
+            c.append(a[0])
+            a.pop(0)
+    while a:
+        c.append(a[0])
+        a.pop(0)
+
+    while b:
+        c.append(b[0])
+        b.pop(0)
+
+    return c
+
+
 def merge_sort(arr):
-    """TODO"""
+    if len(arr) == 1:
+        return arr
+
+    arrayOne = []
+    arrayTwo = []
+
+    for i in range(0, len(arr) // 2):
+        arrayOne.append(arr[i])
+
+    for i in range(len(arr) // 2, len(arr)):
+        arrayTwo.append(arr[i])
+
+    arrayOne = merge_sort(arrayOne)
+    arrayTwo = merge_sort(arrayTwo)
+
+    return merge(arrayOne, arrayTwo)
 
 
 def quick_sort(arr):
     """TODO"""
 
 
+def build_max_heap(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+
+def heapify(arr, n, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    max = i
+    if left < n:
+        if arr[left] > arr[i]:
+            max = left
+        else:
+            max = i
+
+    if right < n:
+        if arr[right] > arr[max]:
+            max = right
+
+    if max != i:
+        arr[i], arr[max] = arr[max], arr[i]
+        heapify(arr, n, max)
+
+
 def heap_sort(arr):
-    """TODO"""
+    build_max_heap(arr)
+    n = len(arr)
+    for i in range(n - 1, -1, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        n = n - 1
+        heapify(arr, n, 0)
 
 
 if __name__ == '__main__':
     random_arrays = []
     n = 10
-    for i in range(5):
+    for i in range(4):
         random_array = random.randint(-10000, 10000, size=n)
         random_arrays.append(random_array)
         n *= 10
     sorting_functions = {'Insertion Sort': insertion_sort, 'Bubble Sort': bubble_sort, 'Selection Sort': selection_sort,
-                         'Merge Sort': merge_sort, 'Quick Sort': quick_sort, 'Heap Sort': heap_sort}
+                         'Merge Sort': merge_sort, 'Quick Sort': quick_sort, 'Heap Sort': heap_sort,
+                         'Built-in Sort': sorted}
     runtimes = dict()
     sizes = []
     for function in sorting_functions.keys():
@@ -58,20 +123,27 @@ if __name__ == '__main__':
             f'\n----------------------------------------------------------------------> ARRAY SIZE:{size} <----------------------------------------------------------------------\n\n')
         for function in sorting_functions.keys():
             temp = random_arrays[i].copy()
-            runtime = dict()
+
             # print(f'Array Before {function}:{temp}\n')
             start_time = time.time()
             sorting_functions[function](temp)
-            runtime[function] = (time.time() - start_time)
-            runtimes[function].append(runtime[function])
+            runtime = (time.time() - start_time)
+            runtimes[function].append(runtime)
+            # if function == 'Merge Sort' or function == 'Built-in Sort':
+            # print(f'Array After {function}:{sorting_functions[function](temp)}\n')
+            # else:
             # print(f'Array After {function}:{temp}\n')
-            print(f'{function} Runtime:{runtime[function]} ms\n')
+            print(f'{function} Runtime:{runtime} s\n')
             print('\n\n\n********************************************************************************\n\n\n')
     print(sizes)
     print(runtimes)
+    i = 0
     for function in sorting_functions.keys():
         pplot.plot(sizes, runtimes[function])
-    pplot.legend(sorting_functions.keys())
+
+    function_names = tuple(sorting_functions.keys())
+    pplot.legend(function_names)
     pplot.xlabel('Array Size')
     pplot.ylabel('Run time (in seconds)')
+    pplot.ylim([0, 1])
     pplot.show()
